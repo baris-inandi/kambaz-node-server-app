1f532d97e742376df313568f5b520b4dd50b8c77
Baris Inandioglu <68742481+baris-inandi@users.noreply.github.com>
2026-04-08 23:04:05 -0400

    init a6

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import session from "express-session";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import AssignmentsRoutes from "./kambaz/assignments/routes.js";
@@ -12,6 +13,10 @@ import ModulesRoutes from "./kambaz/modules/routes.js";
 import UserRoutes from "./kambaz/users/routes.js";
 
 const app = express();
+const CONNECTION_STRING =
+  process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+
+mongoose.connect(CONNECTION_STRING);
 
 app.use(
   cors({
